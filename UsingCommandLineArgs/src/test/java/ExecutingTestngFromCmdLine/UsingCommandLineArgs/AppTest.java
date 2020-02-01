42ffc9878feba884794771d4e66f2bcd512f3918
unkannan <unniqannan@gmail.com>
2020-02-01 01:05:47 +0100

    commnlinr

--- UsingCommandLineArgs/src/test/java/ExecutingTestngFromCmdLine/UsingCommandLineArgs/AppTest.java
+++ UsingCommandLineArgs/src/test/java/ExecutingTestngFromCmdLine/UsingCommandLineArgs/AppTest.java
@@ -1,38 +1,79 @@
 package ExecutingTestngFromCmdLine.UsingCommandLineArgs;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;                                                                                                                                                                                                                                                                                                                                            
+ 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+     
+{ 
+ 
+@Test
+public void WebTableExcercise1() {
+	
+System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");      
+WebDriver driver = new ChromeDriver();
+	driver.get("https://www.techbeamers.com/");                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+	
+	// driver.findElement(By.linkText("Python Tutorial – Beginners")).click();
+	 
+	 List<WebElement> WebTableElement= driver.findElements(By.xpath("//div[@class='main-container']//div[3]//div[1]//div[2]//div[1]/ul"));
+	 for(WebElement e : WebTableElement) {
+			System.out.println(e.getText());
+		}
+	 driver.close();
+}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+@Test
+public void DropDownWithoutSelectClass_WithBootStrapProgramTest() throws IOException {
+	System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+		
+			driver.get("https://www.toolsqa.com/automation-practice-form");
+			driver.manage().window().maximize() ;
+			//Select sel=new Select(driver.findElement(By.id("continents")));
+			//sel.selectByValue("AUS");
+			
+			//BootStrapProgram
+			WebElement cookies=driver.findElement(By.id("cookie_action_close_header"));
+			if(cookies.isDisplayed())
+				cookies.click();
+			driver.findElement(By.id("continents")).click();
+			List <WebElement> dropdown_list =driver.findElements(By.xpath("//select[@id='continents']/option"));
+			System.out.println(dropdown_list.size());
+			
+			for(int i=0;i<dropdown_list.size();i++) {
+				System.out.println(dropdown_list.get(i).getText());
+				if(dropdown_list.get(i).getText().equalsIgnoreCase("South America"))
+					dropdown_list.get(i).click();
+					//break;
+			}
+			driver.quit();
+}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+@Test
+public void GooglePage() {
+	System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+	WebDriver driver=new ChromeDriver();
+	Boolean found = false;
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	driver.get("http://www.google.com");
+	
+	driver.findElement(By.name("q")).sendKeys("Selenium WebDriver Interview questions");
+	//sending keys return 
+	driver.findElement(By.name("q")).sendKeys(Keys.RETURN);
+	driver.quit();
+}
+
 }
