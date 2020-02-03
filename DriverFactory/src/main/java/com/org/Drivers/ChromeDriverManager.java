694c852954a6772605ffb73aba479a0d0e0e4b71
unkannan <unniqannan@gmail.com>
2020-02-03 01:36:26 +0100

    driver

---
+++ DriverFactory/src/main/java/com/org/Drivers/ChromeDriverManager.java
@@ -0,0 +1,26 @@
+package com.org.Drivers;
+
+import org.openqa.selenium.chrome.ChromeDriverService;
+
+public class ChromeDriverManager extends DriverManager{
+
+	private ChromeDriverService chService;
+	@Override
+	protected void startService() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void stopService() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void createDriver() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
