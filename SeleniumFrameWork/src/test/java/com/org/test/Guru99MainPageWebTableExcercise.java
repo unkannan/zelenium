4dc8376bc93e9d03006a28db8cdea922fb64c0b7
unkannan <unniqannan@gmail.com>
2020-02-14 20:06:34 +0100

    Update Guru99MainPageWebTableExcercise.java

--- SeleniumFrameWork/src/test/java/com/org/test/Guru99MainPageWebTableExcercise.java
+++ SeleniumFrameWork/src/test/java/com/org/test/Guru99MainPageWebTableExcercise.java
@@ -21,24 +21,8 @@ public class Guru99MainPageWebTableExcercise extends StartApplication{
 		Guru99MainPage=new Guru99MainPage(driver);
 			
 		Guru99MainPage.WebTableElementsCheck();
-			List<WebElement> WebTableElement=driver.findElements(By.xpath("//html//body//center//table//tbody/tr/td"));
 			
-			for(WebElement e : WebTableElement) {
-				System.out.println(e.getText());
-				if(e.getText().equalsIgnoreCase("second cell"))
-					System.out.println(e.getText());
-			}
-			
-			List list=driver.findElements(By.xpath("//html//body//center//table//tbody/tr"));
-				int count=list.size();
 				
-				System.out.println(list.size());
-				
-				for(int i=0;i<count;i++) {
-					String svalue=list.get(i).toString();
-				}
-				driver.close();	
-				driver.quit();
 		}
 		
 		@Test
