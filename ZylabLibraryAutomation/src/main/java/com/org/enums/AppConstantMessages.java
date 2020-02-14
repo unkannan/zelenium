295fc5500bba1c2f1d0fe7872d1c8e56cba68f80
unkannan <unniqannan@gmail.com>
2020-02-14 17:20:33 +0100

    zylab

---
+++ ZylabLibraryAutomation/src/main/java/com/org/enums/AppConstantMessages.java
@@ -0,0 +1,14 @@
+package com.org.enums;
+/*
+ * These are the warning messages/ error messages which user might see for for invalid inputs.
+ * These messages are used to do the validations/verifications
+ * */
+ 
+
+public class AppConstantMessages {
+
+	public static final long IMPLICIT_WAIT_TIME = 10;
+	public static final long EXPLICIT_WAIT_TIME = 5;
+	
+	
+}
