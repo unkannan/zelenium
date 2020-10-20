a9acfa01b80989690a93514e5b1c9c52d8fc61a6
unkannan <unniqannan@gmail.com>
2020-10-20 16:09:11 +0200

    Oct 21

--- TestSeleniumPlain/src/test/java/APITesting/RestAPI.java
+++ TestSeleniumPlain/src/test/java/APITesting/RestAPI.java
@@ -1,5 +1,7 @@
 package APITesting;
 
+import java.util.HashMap;
+
 import org.apache.xmlbeans.XmlObject;
 import org.testng.annotations.Test;
 
@@ -10,6 +12,7 @@ import io.restassured.http.Method;
 import io.restassured.path.xml.XmlPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.json.JSONObject;
 
 public class RestAPI {
 	
@@ -17,7 +20,7 @@ public class RestAPI {
     
 	 private String text;
 
-	@Test
+	//@Test
 	 public void GetStatusCodeForGoogleCOM()
 	 {
 		 String baseURI="http://www.google.com";
@@ -26,35 +29,107 @@ public class RestAPI {
 		 		System.out.println(response.getStatusCode());
 		 		System.out.println(response.getBody().asString());
 	 }
+	//@Test
+	public void GetEmployeeData() {
+		 String baseURI="http://dummy.restapiexample.com/api/v1/employee/21";
+	 		
+	 		RestAssured.baseURI=baseURI;
+	 		Response response=RestAssured.given().get();
+	 		
+	 		System.out.println(response.getStatusCode());
+	 		System.out.println(response.getBody().asString());
+	}
+	//@Test
+		 public void PostRequestRestAssured()
+		 {
+			 String baseURI="http://dummy.restapiexample.com/api/v1/create";
+			 		
+			 		RestAssured.baseURI=baseURI;
+			 		Response response=RestAssured.given().body("{\"name\":\"name_001\",\"salary\":\"53001\",\"age\":\"23\"}" + "").post();
+			 		System.out.println(response.getStatusCode());
+			 		System.out.println(response.getBody().asString());
+		 }
+		 
+	
+	// @Test
+	 public void UpdateRequestForRecord() {
+		 String  baseURI="http://dummy.restapiexample.com/api/v1/update/21";
+		 
+		 RestAssured.baseURI=baseURI;
+		 Response response=RestAssured.given().body("{\"name\":\"name_001\",\"salary\":\"53001\",\"age\":\"23\"}" + "").put();
+		 
+		 System.out.println(response.getStatusCode());
+		 System.out.println(response.getBody().asString());
+	 }
 	 
 	// @Test
-	 public void PostRestAPItest1()
-	 {   
-	 // Specify the base URL to the RESTful web service
-	 RestAssured.baseURI = "http://phvparser-test.emea.eu.int/vetadr/resources/validate";
-	 
-	 // Get the RequestSpecification of the request that you want to sent
-	 // to the server. The server is specified by the BaseURI that we have
-	 // specified in the above step.
-	 RequestSpecification httpRequest = RestAssured.given();
-	 httpRequest.header("Content-Type", "application/xml;charset=UTF-8");
-	 XmlObject requestParams=(XmlObject) new XmlPath(text);
-	 httpRequest.body(requestParams.toString());
-
-	 
-	 // Make a request to the server by specifying the method Type and the method URL.
-	 // This will return the Response from the server. Store the response in a variable.
-	 Response response = httpRequest.request(Method.POST, "/feed.json");
-	 
-	 
-	 // Now let us print the body of the message to see what response
-	 // we have recieved from the server
-	// String responseBody = response.getBody().asString();
-	// System.out.println("Response Body is =>  " + responseBody);
-		 int responseBody = response.getStatusCode();
-		 System.out.println("Response Body is =>  " + responseBody);
+	 public void DeleteRequestForRecord() {
+		 String  baseURI="http://dummy.restapiexample.com/api/v1/delete/21";
 		 
-		 System.out.println(response.asString());
+		 RestAssured.baseURI=baseURI;
+		 Response response=RestAssured.given().delete();
+		 
+		 System.out.println(response.getStatusCode());
+		 System.out.println(response.getBody().asString());
 	 }
+	 
+	 //------------------------------------------------------------------------------------------------------------------------
+	 //@Test
+	 public void GetWhetherDetailsCity()
+	 {
+		 String  baseURI="https://maps.googleapis.com";
+		 
+		 RestAssured.baseURI=baseURI;
+		 RestAssured.basePath="/maps/api";
+		 
+		 Response response=RestAssured.given().param("origins", "Boston,MA")
+				 			.param("destinations","Lexington,MA")
+				 			.param("key")
+				 			.when()
+				 				.get("/distancematrix/json");
+		 
+		 System.out.println(response.getStatusCode());
+		 System.out.println(response.body().prettyPrint());
+	 }
+	 
+	 
+	 
+	 //@Test
+	 public void ReqresURL_GetSingleUser()
+	 {
+		 String baseURI="https://reqres.in/api/users/2";
+		 		
+		
+		 		RestAssured.baseURI=baseURI;
+		 		Response response=RestAssured.given()
+		 					.contentType("application/json")
+		 					.get();
+		 		System.out.println(response.getStatusCode());
+		 		System.out.println(response.getBody().asString());
+	 }
+	 
+	 @SuppressWarnings("unchecked")
+	@Test
+	 public void ReqresURL_CreateUserPost()
+	 {
+		 String baseURI="https://reqres.in/api/users";
+		 
+		 JSONObject json=new JSONObject();
+		 json.put("name","morpheus");
+		 json.put("job","leader");
+		
+		 		RestAssured.baseURI=baseURI;
+		 		Response response=RestAssured.given()
+		 					.contentType("application/json")
+		 					.body(json.toString())
+		 					.post();
+		 		System.out.println(response.getStatusCode());
+		 		System.out.println(response.getBody().asString());
+		 		 
+		 		 JSONObject jsonResponseResult=new JSONObject(response.getBody().asString()); 
+		 		System.out.println(jsonResponseResult.get("name"));
+	 }
+	 
+	 
 
 }
